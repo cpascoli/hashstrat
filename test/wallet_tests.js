ffd3bf4f9103d07d696826d08b741df27b604c5f
Carlo Pascoli <carlo.pascoli@gmail.com>
2022-05-17 18:19:23 +0100

    Fix wallet tests

--- test/wallet_tests.js
+++ test/wallet_tests.js
@@ -3,44 +3,28 @@ const truffleAssert = require("truffle-assertions")
 const USDCP = artifacts.require("USDCP")
 const Wallet = artifacts.require("Wallet")
 
-
 contract("Wallet", accounts => {
 
     const defaultAccount = accounts[0]
+    let wallet
+    let usdcp
 
     beforeEach(async () => {
-        // let wallet = await Wallet.deployed()
-        // wallet.reset()
+        usdcp = await USDCP.new(web3.utils.toWei('1000', 'ether'))
+        wallet = await Wallet.new(usdcp.address, {from: defaultAccount});
     })
 
 
     it("deposit USDCP tokens into the wallet should increase the account balance", async () => {
-        let wallet = await Wallet.deployed()
-        let usdcp = await USDCP.deployed()
-
         let balanceBefore = await wallet.getBalance()
         assert.equal(balanceBefore, 0, "Account should have no balance")
 
-        const amount = web3.utils.toWei('1', 'ether')
-
-        // deposit 100 USDCP 
-        let depositAmount = 100
+        // deposit 1 USDCP 
+        let depositAmount =  web3.utils.toWei('1', 'ether')
         await usdcp.approve(wallet.address, depositAmount)
-        console.log("approved", depositAmount)
 
-        console.log("depositing", depositAmount)
-
-        const totalSupply = await usdcp.totalSupply()
-        console.log("totalSupply", totalSupply.toString())
-
-        const balance = await usdcp.balanceOf(defaultAccount)
-        console.log("balance", balance.toString())
-        
-        const allowance = await usdcp.allowance(defaultAccount, wallet.address)
-        console.log("allowance", allowance.toString())
-        
         await wallet.deposit(depositAmount, {
-            from: defaultAccount,  gas: 5000000, gasPrice: 500000000
+            from: defaultAccount, gas: 5000000, gasPrice: 500000000
         })
 
         console.log("deposited")
@@ -49,42 +33,38 @@ contract("Wallet", accounts => {
         assert.equal(balanceAfter, depositAmount , "Account should have expected token balance")
     })
 
-    // it("withdraw CaleLP tokens from the wallet should reduce the account balance", async () => {
-    //     let wallet = await Wallet.deployed()
-    //     let usdcp = await USDCP.deployed()
 
-    //     let balance = await wallet.getBalance()
-    //     assert.equal(balance, 0, "Account should have no balance")
+    it("withdraw USDCP tokens from the wallet should reduce the account balance", async () => {
 
-    //     // deposit 100 USDCP 
-    //     let depositAmount = 100
-    //     await usdcp.approve(wallet.address, depositAmount)
-    //     await wallet.deposit(depositAmount)
+        let balance = await wallet.getBalance()
+        assert.equal(balance, 0, "Account should have no balance")
 
-    //     // withdraw
-    //     await wallet.withdraw(depositAmount)
+        // deposit 100 USDCP 
+        let depositAmount = 100
+        await usdcp.approve(wallet.address, depositAmount)
+        await wallet.deposit(depositAmount)
 
-    //     let balanceAfter = await wallet.getBalance()
-    //     assert.equal(balanceAfter, 0 , "Account should have no token after withdraw")
+        // withdraw
+        let withdrawAmount = 30
+        await wallet.withdraw(withdrawAmount)
 
-    // })
+        let balanceAfter = await wallet.getBalance()
+        assert.equal(balanceAfter, depositAmount - withdrawAmount , "Account should have 70 token after withdraw")
 
-    // it("attempting to withdraw more CaleLP tokens than available in balance should throw", async () => {
-    //     let wallet = await Wallet.deployed()
-    //     let usdcp = await USDCP.deployed()
+    })
 
-    //     let balance = await wallet.getBalance()
-    //     assert.equal(balance, 0, "Account should have no balance")
+    it("attempting to withdraw more CaleLP tokens than available in balance should throw", async () => {
+        let balance = await wallet.getBalance()
+        assert.equal(balance, 0, "Account should have no balance")
 
-    //     // deposit 100 USDCP 
-    //     let depositAmount = 100
-    //     await usdcp.approve(wallet.address, depositAmount)
-    //     await wallet.deposit(depositAmount)
+        // deposit 100 USDCP 
+        let depositAmount = 100
+        await usdcp.approve(wallet.address, depositAmount)
+        await wallet.deposit(depositAmount)
 
-    //     await truffleAssert.reverts(
-    //           wallet.withdraw(depositAmount + 1)
-    //     )
-
-    // })
+        await truffleAssert.reverts(
+              wallet.withdraw(depositAmount + 1)
+        )
+    })
 
 })
